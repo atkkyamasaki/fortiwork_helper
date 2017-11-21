8ab9bd3801127a036a504dce7e8ee3ac0fbc5dfc
tyamasaki <tyamasaki@fortinet.com>
2017-11-22 08:42:18 +0900

    ・FortiCare用プラグイン機能の移行 ・mantis bug_view_page 用新規機能追加 　（付箋、ページ移動）

---
+++ js/event.js
@@ -0,0 +1,35 @@
+if (messageColor == undefined) {
+	var messageColor = true;
+}
+if (productSummary == undefined) {
+	var productSummary = true;
+}
+if (customerSummary == undefined) {
+	var customerSummary = true;
+}
+if (duration == undefined) {
+	var duration = true;
+}
+if (durationFooter == undefined) {
+	var durationFooter = true;
+}
+if (unclosedFilter == undefined) {
+	var unclosedFilter = true;
+}
+if (closedMessageColor == undefined) {
+	var closedMessageColor = true;
+}
+if (closedSummary == undefined) {
+	var closedSummary = true;
+}
+
+chrome.storage.sync.set({
+	"messageColor": messageColor,
+	"productSummary": productSummary,
+	"customerSummary": customerSummary,
+	"duration": duration,
+	"durationFooter": durationFooter,
+	"unclosedFilter": unclosedFilter,
+	"closedMessageColor": closedMessageColor,
+	"closedSummary": closedSummary
+});
